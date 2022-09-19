ff4c7726ff4985f7edee90d98584369b856f5bbd
Akombo Neville Akwo <akomboneville55@gmail.com>
2022-09-19 15:36:09 +0100

    persist description field

--- Modules/Product/Resources/views/create.blade.php
+++ Modules/Product/Resources/views/create.blade.php
@@ -70,6 +70,8 @@
                     <textarea style="height: 300px;font-size: 20px" name="description" cols="10"
                               class="html-editor form-control"
                               placeholder="{{trans('vendor.add_product_description_label')}}">
+                                                        {{@old('description')}}
+
                     </textarea>
                 </div>
 
